12985084e1310da3de2418b82f55a214749626fe
Castelias <62962608+Castelias@users.noreply.github.com>
2022-04-07 14:29:45 +0200

    Dodaj pliki projektów.

---
+++ AOC_Day6/Fish.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC_Day6
+{
+    class Fish
+    {
+
+        public int timeToReproduce = 8;
+
+        public bool isNew = false;
+
+        public Fish()
+        {
+            isNew = true;
+        }
+
+         public bool TryToReproduce()
+        {
+            if (this.timeToReproduce == 0)
+            {
+                this.timeToReproduce = 6;
+                this.isNew = true;
+                return true;
+            }
+            else return false;
+        }
+
+        public void DecreaseTimeToReproduce()
+        {
+            this.timeToReproduce--;
+        }
+    }
+}
---
+++ AOC_Day6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC_Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\piotr\source\repos\AOC_Day6\AOC_Day6\inputFile.txt";
+            List<string> inputList = File.ReadAllLines(path).ToList();
+
+            List<int> startingFishList = new List<int>();
+
+            List<Fish> fishList = new List<Fish>();
+            Regex regx = new Regex(@"[0-9]");
+            MatchCollection matches = regx.Matches(inputList[0]);
+
+            foreach (Match match in matches)
+            {
+                startingFishList.Add(Convert.ToInt32(match.Value));
+                fishList.Add(new Fish());
+            }
+
+            for (int i = 0; i < startingFishList.Count; i++)
+            {
+                fishList[i].timeToReproduce = startingFishList[i];
+                fishList[i].isNew = false;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {                
+                for (int j = 0; j < fishList.Count; j++)
+                {
+                    if(fishList[j].TryToReproduce())
+                    {
+                        fishList.Add(new Fish());
+                    }
+                    if(!fishList[j].isNew)
+                    {
+                        fishList[j].DecreaseTimeToReproduce();
+                    }                     
+                    fishList[j].isNew = false;                                      
+                   // Console.Write(fishList[j].timeToReproduce + ", ");
+                }
+                //  Console.WriteLine(" ");
+
+            }
+            Console.WriteLine(fishList.Count + " <- Liczba rybek");
+        }
+    }
+}
